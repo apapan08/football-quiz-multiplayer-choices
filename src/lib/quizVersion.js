88f45c7c8eb4747dc53deb8be0aca1716b05f45f
Angelos Papanastasiou <aggelospapanastasiou905@gmail.com>
2025-10-17 16:37:39 +0300

    update version

--- src/lib/quizVersion.js
+++ src/lib/quizVersion.js
@@ -1,3 +1,3 @@
 // Central quiz version identifier.
 // Change this (e.g., "default-v2" or "2025-09-19") and redeploy to start fresh.
-export const QUIZ_ID = "default-v27";
+export const QUIZ_ID = "default-v28";
